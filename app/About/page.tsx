82d19a40d7addc2339d97c2556651ef3605130fa
herath98 <harshaudayanga401@gmail.com>
2024-10-28 21:45:35 +0530

    changes

---
+++ app/About/page.tsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold mb-6">About</h1>
+      <div className="max-w-2xl">
+        <p className="mb-4">
+          Welcome to our About page. This is where you can learn more about us and our mission.
+        </p>
+        <p className="mb-4">
+          We are dedicated to creating meaningful experiences and delivering value to our users.
+        </p>
+        <p>
+          Feel free to explore our site and get in touch if you have any questions.
+        </p>
+      </div>
+    </div>
+  );
+}
